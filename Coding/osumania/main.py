6a83a38b9a7cb92fa8dc7fe545003fa1d51d4fe8
Spilios06 <76039634+Spilios06@users.noreply.github.com>
2022-09-30 23:06:35 +0300

    Add files via upload

---
+++ Coding/osumania/main.py
@@ -0,0 +1,18 @@
+length = 13
+obj = []
+
+def lines(line):
+    inp = line
+    return inp
+
+def mklist():
+    for j in range(length):
+        column = []
+        for i in range(length):
+            column.append(lines(""))
+        obj.append(column)
+
+
+def outp():
+    answere = 1
+    print(answere)
