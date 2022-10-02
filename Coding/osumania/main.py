4cd8e2c6352050ccc3540bcd9896e27b334a67b9
Spilios06 <76039634+Spilios06@users.noreply.github.com>
2022-10-02 10:09:08 +0300

    Update main.py

--- Coding/osumania/main.py
+++ Coding/osumania/main.py
@@ -1,21 +1,29 @@
 import fileinput
 
-def lines():
-    for i in fileinput.input():
-        obj = []
-        obj.append(line)
-        return obj
+obj = []
 
-def mklist():
-    for j in range(length):
-        column = []
-        for i in range(length):
-            column.append(lines(""))
-        obj.append(column)
+for l in fileinput.input():
+    obj.append(l)
 
+colums = int(obj[0], 10)
 
-def outp():
-    answere = 1
+map = lines[1:]
+map = [line.strip() for line in map]
+
+answere = 0
+for c in range(1, len(map[0]) - 1):
+    holding = False
+    for r in range(colums):
+        if map[r][c] == '-':
+            if holding:
+                holding = False
+                continue
+            else:
+                answere += 1
+
+                if r + 1 < colums and map[r + 1][c] == '#':
+                    holding = True
+
+                continue
+
     print(answere)
-
-lines()
