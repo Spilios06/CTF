48581fce7fbd71bb0c8676afb5faa3a6508d660d
Spilios06 <76039634+Spilios06@users.noreply.github.com>
2022-12-07 09:04:55 +0000

    typo

--- Coding/osumania/main.py
+++ Coding/osumania/main.py
@@ -10,7 +10,7 @@ colums = int(obj[0], 10)
 map = lines[1:]
 map = [line.strip() for line in map]
 
-answere = 0
+answer = 0
 for c in range(1, len(map[0]) - 1):
     holding = False
     for r in range(colums):
@@ -19,11 +19,11 @@ for c in range(1, len(map[0]) - 1):
                 holding = False
                 continue
             else:
-                answere += 1
+                answer += 1
 
                 if r + 1 < colums and map[r + 1][c] == '#':
                     holding = True
 
                 continue
 
-    print(answere)
+    print(answer)
